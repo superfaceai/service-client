cae84ff1edf11b88ef44d062585382f0ad12da76
Jan Halama <mail@janhalama.cz>
2021-04-08 11:30:48 +0200

    fix: add missing export to index file

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,4 @@ export * from './auth_token';
 export * from './client_options';
 export * from './refresh_access_token_options';
 export * from './passwordless_verify_response';
+export * from './passwordless_verify_options';
