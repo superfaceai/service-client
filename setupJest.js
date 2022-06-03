3b595538b563519fec9f0ae689f658bdd7b1ff0a
Jan Halama <mail@janhalama.cz>
2022-06-03 15:38:38 +0200

    chore: update cross-fetch to v3.1.5

--- setupJest.js
+++ setupJest.js
@@ -1,1 +1,9 @@
-require('jest-fetch-mock').enableMocks();
+jest.mock('cross-fetch', () => {
+  const fetchMock = require('jest-fetch-mock');
+  const originalFetch = jest.requireActual('cross-fetch');
+  return {
+    __esModule: true,
+    ...originalFetch,
+    default: fetchMock,
+  };
+});
